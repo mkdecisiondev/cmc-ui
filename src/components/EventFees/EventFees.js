450b15f862b69e434b1e35130f56e042c84adf56
Justin Pi <pi.jp.314@gmail.com>
2018-05-10 18:04:56 -0700

    event fees update

--- src/components/EventFees/EventFees.js
+++ src/components/EventFees/EventFees.js
@@ -7,46 +7,45 @@ Object.assign(MkComponent._mkComponentConstructors, {
 
 class EventFees extends MkComponent {
 	init () {
-		this.annualFee = document.getElementsByName('annualFee');
-		this.lifetimeFee = document.getElementsByName('lifetimeFee');
 		this.membershipFees = 0;
 	}
 
 	registerEventHandlers () {
 		this.node.addEventListener('change', this.handleFeesChange.bind(this));
-		document.getElementById('annualFee').addEventListener('change', this.handleAnnual.bind(this));
-		document.getElementById('lifetimeFee').addEventListener('change', this.handleLifetime.bind(this));
+		this.annualFeeToggle.addEventListener('change', this.handleAnnual.bind(this));
+		this.lifetimeFeeToggle.addEventListener('change', this.handleLifetime.bind(this));
 	}
 
 	handleFeesChange () {
-		this.totalFees = this.adultFullNumberBox.total + this.adultOneDayNumberBox.total + this.adultTwoDayNumberBox.total + this.residentsNumberBox.total + this.spouseNumberBox.total + this.childrenNumberBox.total + this.guestsNumberBox.total + this.membershipFees;
+		this.totalFees = this.adultFullNumberBox.total +
+		this.adultOneDayNumberBox.total +
+		this.adultTwoDayNumberBox.total +
+		this.residentsNumberBox.total +
+		this.spouseNumberBox.total +
+		this.childrenNumberBox.total +
+		this.guestsNumberBox.total +
+		this.membershipFees;
 
 		this.total.textContent = this.totalFees;
 	}
 
 	handleAnnual () {
-		for (let i = 0, length = this.annualFee.length; i < length; i++) {
-			if (this.annualFee[i].checked) {
-				if (this.annualFee[i].value === 'true') {
-					this.lifetimeFee[1].click();
-					this.membershipFees = 100;
-					this.handleFeesChange();
-				}
-				break;
-			}
+		if (this.annualFeeOn.checked) {
+			this.lifetimeFeeOff.click();
+			this.membershipFees = 100;
+		}
+		else {
+			this.membershipFees = 0;
 		}
 	}
 
 	handleLifetime () {
-		for (let i = 0, length = this.lifetimeFee.length; i < length; i++) {
-			if (this.lifetimeFee[i].checked) {
-				if (this.lifetimeFee[i].value === 'true') {
-					this.annualFee[1].click();
-					this.membershipFees = 500;
-					this.handleFeesChange();
-				}
-				break;
-			}
+		if (this.lifetimeFeeOn.checked) {
+			this.annualFeeOff.click();
+			this.membershipFees = 500;
+		}
+		else {
+			this.membershipFees = 0;
 		}
 	}
 }
