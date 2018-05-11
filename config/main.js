4418a7bee100c101a517f131c602880162df2912
Justin Pi <pi.jp.314@gmail.com>
2018-05-11 13:48:19 -0700

    added states and country to config

--- config/main.js
+++ config/main.js
@@ -1,11 +1,15 @@
 const lang = require('./lang/default');
+const country = require('./country.js');
 const roles = require('./roles.js');
 const siteLinks = require('./siteLinks.js');
 const socialMedia = require('./socialMedia');
+const states = require('./states.js');
 
 module.exports = {
 	lang,
+	country,
 	roles,
 	siteLinks,
 	socialMedia,
+	states,
 };
