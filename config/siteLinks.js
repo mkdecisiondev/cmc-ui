42524dc26c3081a2dbb02028b06dbaa469c15cd7
avram <adkantor@gmail.com>
2018-05-15 09:38:22 -0700

    to show current nav item, added classes to target on corresponding pages

--- config/siteLinks.js
+++ config/siteLinks.js
@@ -4,21 +4,26 @@ module.exports = [
 	{
 		title: lang.home,
 		url: '/',
+		class: 'home',
 	},
 	{
 		title: lang.events,
 		url: '/events.html',
+		class: 'events',
 	},
 	{
 		title: lang.registration,
 		url: '/registration.html',
+		class: 'registration',
 	},
 	{
 		title: lang.photos,
 		url: '/photos.html',
+		class: 'photos',
 	},
 	{
 		title: lang.contactUS,
 		url: '/contact.html',
+		class: 'contact',
 	},
 ];
