0917d4b7264e6cfade2e30202f0d44ec97c04891
leprekhanh <khanh.hoang@live.com>
2018-05-15 14:02:19 -0700

    Rename files

--- config/main.js
+++ config/main.js
@@ -1,17 +1,17 @@
 const lang = require('./lang/default');
 const countries = require('./countries.js');
+const eventFeesQuantity = require('./eventFeesQuantity.js');
 const roles = require('./roles.js');
 const siteLinks = require('./siteLinks.js');
 const socialMedia = require('./socialMedia');
 const states = require('./states.js');
-const quantity = require('./quantity.js');
 
 module.exports = {
 	lang,
 	countries,
+	eventFeesQuantity,
 	roles,
 	siteLinks,
 	socialMedia,
 	states,
-	quantity,
 };
