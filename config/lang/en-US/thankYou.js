fddf57bddc408f3c0020985ac32f857a40145c55
avram <adkantor@gmail.com>
2018-05-15 11:09:44 -0700

    made var name more descriptive

--- config/lang/en-US/thankYou.js
+++ config/lang/en-US/thankYou.js
@@ -1,5 +1,5 @@
 module.exports = {
 	thanksForRegistering: 'Thank You for registering',
-	thanksForRegisteringEtc: 'Thank you for registering for the upcoming CMC Alumni Association event!',
+	thanksForRegisteringForEvents: 'Thank you for registering for the upcoming CMC Alumni Association event!',
 	confirmationWillBeEmailed: 'Your Registration Confirmation will be emailed to you shortly.',
 };
