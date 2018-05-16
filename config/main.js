1e19e2316ce39d821c81fab3c863d2b80d96738f
Justin Pi <30294837+billShoe@users.noreply.github.com>
2018-05-16 15:45:18 -0700

    Registration From Update (#53)

--- config/main.js
+++ config/main.js
@@ -1,6 +1,5 @@
 const lang = require('./lang/default');
 const countries = require('./countries.js');
-const eventFeesQuantity = require('./eventFeesQuantity.js');
 const roles = require('./roles.js');
 const siteLinks = require('./siteLinks.js');
 const socialMedia = require('./socialMedia');
@@ -9,7 +8,6 @@ const states = require('./states.js');
 module.exports = {
 	lang,
 	countries,
-	eventFeesQuantity,
 	roles,
 	siteLinks,
 	socialMedia,
