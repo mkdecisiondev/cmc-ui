fabf66ac27bdfa1b615784234c5d54822b1f318c
andrewglevy <andrewglevy@gmail.com>
2018-05-15 18:54:46 -0700

    add stripe elements to form

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -4,6 +4,7 @@ import { forms } from '../form/forms.js';
 import { MkComponent } from '../MkComponent/MkComponent.js';
 import { Select } from '../Select/Select.js';
 import { TextBox } from '../TextBox/TextBox.js';
+import { FormManager } from '../FormManager/FormManager.js';
 
 Object.assign(MkComponent._mkComponentConstructors, {
 	Dialog,
@@ -11,18 +12,34 @@ Object.assign(MkComponent._mkComponentConstructors, {
 	Select,
 	TextBox,
 });
+const stripe = Stripe('pk_test_jZZnE8X4va0KaR2T7McLcaC2');
 
 class RegistrationForm extends MkComponent {
-	init () {
+	init() {
+		const elements = stripe.elements({
+			fonts: [
+				{
+					cssSrc: 'https://fonts.googleapis.com/css?family=Source+Code+Pro',
+				},
+			],
+			locale: window.__exampleLocale,
+		});
+		const cardNumber = elements.create('cardNumber');
+		const cardExpiry = elements.create('cardExpiry');
+		const cardCvc = elements.create('cardCvc');
+		cardNumber.mount('#cardNumber');
+		cardExpiry.mount('#cardExpiry');
+		cardCvc.mount('#cardCvc');
 		this.registrationFeesDialog.closeable = true;
 
 		/* eslint-disable no-magic-numbers */
 		forms.constrainInput({
 			inputNode: this.phoneNumberTextBox.inputNode,
-			allow: [ '-' ],
-			positions: [ 3, 7 ],
+			allow: ['-'],
+			positions: [3, 7],
 		});
 		/* eslint-enable no-magic-numbers */
+		this.form = new FormManager(this.node);
 	}
 
 	registerEventHandlers () {
@@ -49,8 +66,16 @@ class RegistrationForm extends MkComponent {
 		this.registrationFeesDialog.show();
 	}
 
-	handleSubmit () {
-		window.location.href = '/thank-you.html';
+	handleSubmit (event) {
+		event.preventDefault();
+		this.formData = this.form.value;
+		console.log(this.formData);
+		// console.log(elements);
+		const stripeToken = stripe.createToken(this.formData).then((result) => {
+			return result;
+		});
+		console.log(stripeToken);
+		// window.location.href = '/thank-you.html';
 	}
 }
 
