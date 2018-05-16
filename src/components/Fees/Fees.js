7a8a1e3e7c83aa3d0882d7154b058d44f8fce130
leprekhanh <khanh.hoang@live.com>
2018-05-16 11:49:18 -0700

    RegistrationForm: remove inputs TextBox: update variables

--- src/components/Fees/Fees.js
+++ src/components/Fees/Fees.js
@@ -7,51 +7,18 @@ Object.assign(MkComponent._mkComponentConstructors, {
 
 class Fees extends MkComponent {
 	init () {
-		this.membershipFees = 0;
 		this.eventFees = 0;
 	}
 
 	registerEventHandlers () {
-		this.annualFeeToggle.addEventListener('change', this.handleAnnual.bind(this));
-		this.lifetimeFeeToggle.addEventListener('change', this.handleLifetime.bind(this));
 		this.eventFeesNode.addEventListener('change', this.updateTotalFees.bind(this));
 		this.submitButton.addEventListener('click', this.validateEventFees.bind(this));
 	}
 
 	updateTotalFees () {
-		this.eventFees = this.adultOneDayNumberBox.total +
-		this.adultTwoDayNumberBox.total +
-		this.residentsNumberBox.total +
-		this.spouseNumberBox.total +
-		this.childrenNumberBox.total +
-		this.guestsNumberBox.total;
-
-		this.totalNode.textContent = this.eventFees + this.membershipFees;
+		this.eventFees = this.adultNumberBox.total + this.childrenNumberBox.total;
+		this.totalNode.textContent = this.eventFees;
 		this.validateEventFees();
-	}
-
-	handleAnnual () {
-		if (this.annualFeeOn.checked) {
-			this.lifetimeFeeOff.click();
-			this.membershipFees = 100;
-		}
-		else {
-			this.membershipFees = 0;
-		}
-
-		this.totalNode.textContent = this.eventFees + this.membershipFees;
-	}
-
-	handleLifetime () {
-		if (this.lifetimeFeeOn.checked) {
-			this.annualFeeOff.click();
-			this.membershipFees = 500;
-		}
-		else {
-			this.membershipFees = 0;
-		}
-
-		this.totalNode.textContent = this.eventFees + this.membershipFees;
 	}
 
 	validateEventFees (event) {
