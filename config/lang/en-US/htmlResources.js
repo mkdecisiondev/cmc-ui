45b127f2e26c0d8a10d7100efae07d1974ab3484
Justin Pi <pi.jp.314@gmail.com>
2018-05-22 17:32:50 -0700

    update htmlResource

--- config/lang/en-US/htmlResources.js
+++ config/lang/en-US/htmlResources.js
@@ -1,6 +1,6 @@
 const requireText = require('require-text');
 
 module.exports = {
-	privacyPolicyContent: requireText('./privacyPolicyContent.html', require),
 	eventContent: requireText('./eventContent.html', require),
+	privacyPolicyContent: requireText('./privacyPolicyContent.html', require),
 };
